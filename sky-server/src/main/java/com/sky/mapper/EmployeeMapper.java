5a186abcd2a2563a9b7661aed119ee629ecb1a22
zhenyunzhu <zzy1322388195@163.com>
2026-09-05 14:25:32 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -15,4 +16,10 @@ public interface EmployeeMapper {
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
+    /**
+     *插入员工数据
+     * @param employee
+     */
+    @Insert("insert into employee (id, username, password, name, phone,sex,id_number, create_time, update_time, create_user, update_user) values (#{id}, #{username}, #{password}, #{name}, #{phone}, #{sex}, #{idNumber}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
+    void insert(Employee employee);
 }
